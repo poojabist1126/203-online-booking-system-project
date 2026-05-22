457637d3f99abe021b074cbf89f470775fc8bbd4
Pooja Bist <270585237@yoobeestudent.ac.nz>
2026-05-22 13:06:27 +1200

    Some updates

--- app.py
+++ app.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template
+import flask
+app = flask.Flask(__name__)
 
-app= Flask(__name__)
+tutors = [
+    {
+        "id": 1, 
+        "name": "James", 
+        "major": "Computer Science", 
+        "rate": 30,
+        "profile_pic": "static/James.png"
+    },
+    {
+        "id": 2, 
+        "name": "Lisa", 
+        "major": "English", 
+        "rate": 28,
+        "profile_pic": "static/Lisa.png"
+    },
+    
+ 
+     {
+        "id": 3,
+        "name": "Michael",
+        "major": "Mathematics",
+        "rate": 35,
+        "profile_pic": "static/Michael.png"
+        
+    },
+    {
+        "id": 4,
+        "name": "Sarah",
+        "major": "Physics",
+        "rate": 32,
+        "profile_pic": "static/Sarah.png"
+        
+    },
+    {
+        "id": 5,
+        "name": "David",
+        "major": "Chemistry",
+        "rate": 33,
+        "profile_pic": "static/David.png"
+        
+    },
+    {
+        "id": 6,
+        "name": "Emma",
+        "major": "Biology",
+        "rate": 31,
+        "profile_pic": "static/Emma.png"
+       
+    },
+    {
+        "id": 7,
+        "name": "Daniel",
+        "major": "History",
+        "rate": 28,
+        "profile_pic": "static/Daniel.png"
+       
+    },
+]
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return flask.render_template('index.html', tutors=tutors)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
